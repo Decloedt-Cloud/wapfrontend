68b35be8564ccffaad188520e21adfd9b415c0b3
decfakir <fkdc09@gmail.com>
2025-07-24 18:33:35 +0100

    wap-integration-login-forgetpassword-resetpassword-register-intervenants

--- src/pages/JoinWap.jsx
+++ src/pages/JoinWap.jsx
@@ -5,6 +5,9 @@ import { useNavigate } from 'react-router-dom';
 const JoinWap = () => {
 const navigate = useNavigate();
 
+
+
+
   const handleRegisterRedirect = () => {
     navigate('/Registerintervenant');
   };
